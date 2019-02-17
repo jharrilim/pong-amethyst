b6c5ea5c6f7c9b1e1f6cdadb8b878a275cf3441f
Joseph Harrison-Lim <Josephharrisonlim@gmail.com>
2019-02-17 17:50:46 -0500

    Erase default and create window

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,48 @@
 extern crate amethyst;
 
-use amethyst::{
-    prelude::*,
-    renderer::{DisplayConfig, DrawFlat, Pipeline, PosNormTex, RenderBundle, Stage},
-    utils::application_root_dir,
+use amethyst::prelude::*;
+use amethyst::renderer::{
+    DisplayConfig, DrawFlat2D, Event, Pipeline,
+    RenderBundle, Stage, VirtualKeyCode
 };
 
-struct Example;
+use amethyst::utils::application_root_dir;
 
-impl SimpleState for Example {}
+pub struct Pong;
 
+/**
+ * Used by Amethyst's state machine for starting, stopping, and updating the game.
+ *
+ */
+impl SimpleState for Pong {
+
+}
+
+
+// Return result so we can use ? operator
 fn main() -> amethyst::Result<()> {
+    let path = format!("{}/resources/display_config.ron", application_root_dir());
+
+    // Load display_config.ron
+    let config = DisplayConfig::load(&path);
+    // Show logs during runtime
     amethyst::start_logger(Default::default());
 
-    let path = format!(
-        "{}/resources/display_config.ron",
-        application_root_dir()
-    );
-    let config = DisplayConfig::load(&path);
+    // Renders a background
+    let pipe = Pipeline::build()
+        .with_stage(
+            Stage::with_backbuffer()
+                .clear_target([0.0, 0.0, 0.0, 1.0], 1.0)
+                .with_pass(DrawFlat2D::new()),
+        );
 
-    let pipe = Pipeline::build().with_stage(
-        Stage::with_backbuffer()
-            .clear_target([0.00196, 0.23726, 0.21765, 1.0], 1.0)
-            .with_pass(DrawFlat::<PosNormTex>::new()),
-    );
+    let game_data = GameDataBuilder::default()
+        .with_bundle(
+            RenderBundle::new(pipe, Some(config))
+                .with_sprite_sheet_processor()
+        )?;
 
-    let game_data =
-        GameDataBuilder::default().with_bundle(RenderBundle::new(pipe, Some(config)))?;
-    let mut game = Application::new("./", Example, game_data)?;
+    let mut game = Application::new("./", Pong, game_data)?;
 
     game.run();
 
